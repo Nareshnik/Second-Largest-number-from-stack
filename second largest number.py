d0da514e15f63f93451046bd8e991b7055a473b1
Naresh Naik <126505787+Nareshnik@users.noreply.github.com>
2024-11-07 12:53:54 +0530

    Update second largest number.py

--- second largest number.py
+++ second largest number.py
@@ -1,26 +1,14 @@
-x=input("Enter any string: ")
-longest =""
-max_length=0
-n=0
-while n<len(x):
-    m=n+1
-    while m<=len(x):
-        sub=""
-        rev=""
-        i=n
-        while i<m:
-            sub+=x[i]
-            i+=1
-        i=m-1
-        while i>=n:
-            rev+=x[i]
-            i_=1
-        if sub==rev and len(sub)>max_len:
-            longest=sub
-            max_len=len(sub)
-        m+=1
-    n=+1
-    print("longest palindromestringin the given string",longest)  
-
+#second largest number in an array
+ arr=list(map(int,input("Enter an array of numbers: ").split(",")))
+ large=arr[0]
+ secondlarge=arr[0]
+ print("Array : ",arr)
+ for i in arr:
+   if i>large:
+     secondlarge=large
+     large=i
+   elif i>secondlarge and i!=large:
+        secondlarge=i
+ print("Second largest number in array : ",secondlarge
 
 
